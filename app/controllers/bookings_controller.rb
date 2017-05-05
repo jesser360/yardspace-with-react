07de2fd2eac21ab523847706db5b341f255eb8a9
Jesser360 <jesser360@gmail.com>
2017-05-05 13:34:32 -0700

    sent requests page, , delete pending reqs and see answered reqs

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,22 @@
 class BookingsController < ActionController::Base
 
-
 def new
-  @curr_user = User.find_by_id(session[:user_id]) if session[:user_id]
+  @curr_user = User.find_by_id(session[:user_id])
   @booking = Booking.new
   @yard = Yard.find_by_id(params[:yard_id.to_s])
   @host = User.find_by_id(@yard.user.id.to_s)
   @camper= User.find_by_id(session[:user_id])
 end
 
+def destroy
+  @booking = Booking.find_by_id(params[:id])
+  if @booking.destroy
+    redirect_to :back
+    flash[:error] = "Booking for " + @booking.start_date.to_s + " to " + @booking.end_date.to_s + " was deleted"
+  end
+end
 
 def create
-  puts "WERE IN CREATE BOOKINGS"
-  puts "GROUP SIZE: "+ booking_params[:group_size]
-  puts  booking_params[:start_date]
-  puts  booking_params[:end_date]
-  puts "YARD ID: " + params[:yard_id]
-
 @curr_user= User.find_by_id(session[:user_id])
 @booking = Booking.new()
 @yard = Yard.find_by_id(params[:yard_id])
@@ -29,15 +29,22 @@ def create
 @booking.is_answered = false
 @booking.is_accepted = false
 @booking.pets_or_kids = booking_params[:pets_or_kids]
-if @booking.save
-  flash[:success] = "Booking made, check your schedule to confirm details"
-  redirect_to user_path_url(@curr_user)
-else
-  flash[:error]= @booking.errors.full_messages.join(", ")
-  redirect_to :back
+  if @booking.save
+    flash[:success] = "Booking made, check your schedule to confirm details"
+    redirect_to user_path_url(@curr_user)
+  else
+    flash[:error]= @booking.errors.full_messages.join(", ")
+    redirect_to :back
+  end
 end
 
+def sent
+  @curr_user = User.find_by_id(session[:user_id])
+  @answered_bookings = Booking.where(camper_id: @curr_user.id).where(is_answered: true)
+  @pending_bookings = Booking.where(camper_id: @curr_user.id).where(is_answered: false)
 end
+
+private
 
 def create_booking_params
   params.require(:yard_id)
@@ -45,6 +52,10 @@ end
 
 def booking_params
   params.require(:booking).permit(:group_size, :start_date, :end_date, :yard_id, :pets_or_kids)
+end
+
+def destroy_booking_params
+  params.require(:booking_id)
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
   patch 'yards/update/:id', to: 'yards#update', as: 'update_yard_path'
   get 'cities/:city', to: 'yards#cities', as: 'city_path'
 
+  get '/bookings/sent', to: 'bookings#sent', as: 'sent_bookings_path'
   get '/bookings/new', to: 'bookings#new', as: 'new_booking_path'
+  delete '/bookings/destroy/:id', to: 'bookings#destroy', as: 'destroy_booking_path'
+
+
   post '/bookings', to: 'bookings#create', as: 'create_booking_path'
 
   get '/messages/new', to: 'messages#new', as: 'new_message_path'
